842a6d8fffa4b3a7657a60af55101ff1eb911945
GitHub <abduqahhor2349@gmail.com>
2022-01-28 07:24:11 +0500

    Initial Commit

---
+++ MyStart/MyStartApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyStartApp.swift
+//  MyStart
+//
+//  Created by Abduqaxxor on 28/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStartApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
